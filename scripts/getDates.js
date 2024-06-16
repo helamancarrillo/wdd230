fdf554725509f06fa3c2811fea6379877df4b4eb
helamancarrillo <helamancarrillo11@gmail.com>
2024-06-15 23:53:40 -0600

    Course Home Page

---
+++ scripts/getDates.js
@@ -0,0 +1,10 @@
+
+
+
+function displayLastModified() {
+    const lastModified = document.lastModified;
+    const lastModifiedElement = document.getElementById('lastModified');
+    lastModifiedElement.textContent = `Last Modified: ${lastModified}`;
+}
+
+window.onload = displayLastModified;
