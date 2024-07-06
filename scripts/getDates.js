4500c5ba1fa2d257cf566508fe5bf63497bad084
helamancarrillo <helamancarrillo11@gmail.com>
2024-07-06 16:13:24 -0600

    team assignment week03 update

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -44,4 +44,21 @@ modeButton.addEventListener("click", () => {
 
 	}
 });
+
+// Counter Stuff
+
+
+const visitsDisplay = document.querySelector(".visits");
+
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+}
+
+numVisits++;
+
+localStorage.setItem("numVisits-ls", numVisits);
  
