1299d6c3fba3237380dd683891962d89c95031f7
helamancarrillo <helamancarrillo11@gmail.com>
2024-07-09 17:16:03 -0600

    week03 almost done

--- chamber/scripts/nav.js
+++ chamber/scripts/nav.js
@@ -16,3 +16,30 @@ function displayLastModified() {
 }
 
 window.onload = displayLastModified;
+
+
+// local storage
+
+const welcomeMessage = document.getElementById('local-storage');
+
+    if (welcomeMessage) {
+        const lastVisit = localStorage.getItem('lastVisit');
+        const currentVisit = Date.now();
+
+        if (lastVisit) {
+            const daysBetween = Math.floor((currentVisit - lastVisit) / (1000 * 60 * 60 * 24));
+            if (daysBetween < 1) {
+                welcomeMessage.textContent = 'Back so soon! Awesome!';
+            } else if (daysBetween === 1) {
+                welcomeMessage.textContent = 'You last visited 1 day ago.';
+            } else {
+                welcomeMessage.textContent = `You last visited ${daysBetween} days ago.`;
+            }
+        } else {
+            welcomeMessage.textContent = 'Welcome! Let us know if you have any questions.';
+        }
+
+        localStorage.setItem('lastVisit', currentVisit);
+    } else {
+        console.error('Element with id "visit-message" not found. Skipping visit message update.');
+    }
