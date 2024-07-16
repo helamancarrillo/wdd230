1fad6f5a213c591d8a1de70e7688b63ea60df841
helamancarrillo <helamancarrillo11@gmail.com>
2024-07-16 12:42:16 -0600

    join page chamber update

--- chamber/scripts/nav.js
+++ chamber/scripts/nav.js
@@ -3,8 +3,8 @@ const hamButton = document.querySelector('#menu');
 const navElement = document.querySelector('#animateme');
 
 hamButton.addEventListener('click', () => {
-	navElement.classList.toggle('open');
-	hamButton.classList.toggle('open');
+    navElement.classList.toggle('open');
+    hamButton.classList.toggle('open');
 });
 
 //last modified 
@@ -22,24 +22,36 @@ window.onload = displayLastModified;
 
 const welcomeMessage = document.getElementById('local-storage');
 
-    if (welcomeMessage) {
-        const lastVisit = localStorage.getItem('lastVisit');
-        const currentVisit = Date.now();
+if (welcomeMessage) {
+    const lastVisit = localStorage.getItem('lastVisit');
+    const currentVisit = Date.now();
 
-        if (lastVisit) {
-            const daysBetween = Math.floor((currentVisit - lastVisit) / (1000 * 60 * 60 * 24));
-            if (daysBetween < 1) {
-                welcomeMessage.textContent = 'Back so soon! Awesome!';
-            } else if (daysBetween === 1) {
-                welcomeMessage.textContent = 'You last visited 1 day ago.';
-            } else {
-                welcomeMessage.textContent = `You last visited ${daysBetween} days ago.`;
-            }
+    if (lastVisit) {
+        const daysBetween = Math.floor((currentVisit - lastVisit) / (1000 * 60 * 60 * 24));
+        if (daysBetween < 1) {
+            welcomeMessage.textContent = 'Back so soon! Awesome!';
+        } else if (daysBetween === 1) {
+            welcomeMessage.textContent = 'You last visited 1 day ago.';
         } else {
-            welcomeMessage.textContent = 'Welcome! Let us know if you have any questions.';
+            welcomeMessage.textContent = `You last visited ${daysBetween} days ago.`;
         }
-
-        localStorage.setItem('lastVisit', currentVisit);
     } else {
-        console.error('Element with id "visit-message" not found. Skipping visit message update.');
+        welcomeMessage.textContent = 'Welcome! Let us know if you have any questions.';
     }
+
+    localStorage.setItem('lastVisit', currentVisit);
+} else {
+    console.error('Element with id "visit-message" not found. Skipping visit message update.');
+}
+
+// timestamp
+
+var currentDate = new Date;
+
+var currentTimestamp = currentDate.toISOString();
+
+document.getElementById('timestamp').value = currentTimestamp;
+
+
+
+
